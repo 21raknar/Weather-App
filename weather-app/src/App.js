cea34006230bae6c26418aa05b57657e5c97feb6
Andres Manzanal <andresmasaa@hotmail.com>
2020-06-15 12:21:32 +0200

    Sección 13 completa

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import LocationList from "./components/LocationList";
+import LocationListContainer from "./containers/LocationListContainer";
 import { Grid, Col, Row } from "react-flexbox-grid";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
-import ForecastExtended from "./components/ForecastExtended";
+import ForecastExtendedContainer from "./containers/ForecastExtendedContainer";
 
 const cities = [
   "Buenos Aires,ar",
@@ -16,23 +16,9 @@ const cities = [
   "Lima,pe",
   "Berlin",
 ];
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      city: null,
-    };
-  }
-
-  handleSelectedLocation = (city) => {
-    this.setState({
-      city,
-    });
-    console.log(`handleSelectionLocation  ${city}`);
-  };
-
   render() {
-    const { city } = this.state;
     return (
       <Grid>
         <Row>
@@ -46,15 +32,12 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList
-              cities={cities}
-              onSelectedLocation={this.handleSelectedLocation}
-            />
+            <LocationListContainer cities={cities} />
           </Col>
           <Col xs={12} md={6}>
             <Paper elevation={4}>
               <div className="details">
-                {city && <ForecastExtended city={city} />}
+                <ForecastExtendedContainer />
               </div>
             </Paper>
           </Col>
